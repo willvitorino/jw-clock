30a0166ee9f4d2dacbfbebeb1546298acfcd3ad1
Zé William <josewilliam@justto.com.br>
2020-11-11 09:56:53 -0300

    🎉 Innitial commit

---
+++ backend.js
@@ -0,0 +1,29 @@
+const { app, BrowserWindow } = require('electron')
+const configs = require('./configs.json')
+function createWindow () {
+  const win = new BrowserWindow(configs)
+
+  // win.setIgnoreMouseEvents(true)
+  win.setMenu(null)
+
+  win.loadFile('index.html')
+  win.openDevTools()
+
+  win.once('ready-to-show', win.show)
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
+
+
